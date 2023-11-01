bfc5883363931d1c40d3ab1a76fce878405d74de
ThomasMrchl <marchalthomaswork@gmail.com>
2023-11-01 12:22:58 +0100

    Part 1. - createList | insertCell | displayLevel | displayAllLevels | insertSorted

--- main.c
+++ main.c
@@ -3,14 +3,23 @@
 //
 
 #include "cell.h"
-#include <stdio.h>
+#include "list.h"
 
 int main(){
 
-    //Show Part 1 - Functionalities
-    t_d_cell *myCell = createCell(4, 5);
-    printf("%d\n", myCell->value);
-    printf("%d\n", myCell->nb_levels);
+    t_d_list *myList = createList(5);
+
+    t_d_cell *Cell1 = createCell(5, 3);
+    t_d_cell *Cell2 = createCell(8, 2);
+    t_d_cell *Cell3 = createCell(2, 4);
+    t_d_cell *Cell4 = createCell(8, 3);
+
+    insertSorted(myList, Cell1);
+    insertSorted(myList, Cell2);
+    insertSorted(myList, Cell3);
+    insertSorted(myList, Cell4);
+
+    displayAllLevels(myList);
 
     return 0;
 }
