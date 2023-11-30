2bf192a55f9f0d5019a520c859f547d91689cd7c
ThomasMrchl <marchalthomaswork@gmail.com>
2023-11-30 10:03:51 +0100

    [ADD] Create Contact + Display Contacts
    Basic functions over contacts

--- main.c
+++ main.c
@@ -6,30 +6,60 @@
 #include "list.h"
 #include "stdio.h"
 #include "timer.h"
+#include "contact.h"
+#include <string.h>
 
 int main(){
 
-    t_d_list *myList = createList(5);
+    int choice, menu=1;
 
-    insertLevelCells(myList, 100);
+    while(menu){
+        printf("\n");
+        printf("1 - Search a contact\n");
+        printf("2 - View a contact's appointment\n");
+        printf("3 - Create a contact\n");
+        printf("4 - Display contacts\n");
+        printf("4 - Create an appointment\n");
+        printf("5 - Delete an appointment\n");
+        printf("6 - Save appointments\n");
+        printf("7 - Load an appointment file\n");
+        printf("8 - Calculation time example\n");
+        printf("9 - Quit\n");
+        printf("Enter your choice :\n");
+        scanf("%d", &choice);
 
-    int result = searchLevelValue(myList, 52);
+        if (choice == 1){
 
-    if (result == 1){
-        printf("Value found!\n");
-    } else {
-        printf("Value not found!\n");
-    }
+        } else if (choice == 2){
 
-    //algo 2 without levels.
+        } else if (choice == 3){
+            char firstName[50], lastName[50];
+            printf("Enter the first name of your contact\n");
+            scanf(" %s", firstName);
+            printf("Enter the last name of your contact\n");
+            scanf(" %s", lastName);
 
-    int result2 = searchValue(myList, 52);
+            contact *newContact = createContact(firstName, lastName);
 
+        } else if (choice == 4){
+            displayContacts();
+        } else if (choice == 5){
 
-    if (result2 == 1){
-        printf("Value found!\n");
-    } else {
-        printf("Value not found!\n");
+        } else if (choice == 6){
+
+        } else if (choice == 7){
+
+        } else if (choice == 8){
+
+        } else if (choice == 9){
+
+        } else if (choice == 10){
+            printf("See you!");
+            break;
+        } else {
+            printf("Wrong choice!\n");
+        }
+
     }
 
     return 0;
